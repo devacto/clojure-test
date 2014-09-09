05add93222d50870d25e4c12bd1eb5b44e869098
Victor Wibisono <devacto@gmail.com>
2014-09-09 17:50:28 +1000

    Added an object to store things in memory.

--- src/clojure_test/handler.clj
+++ src/clojure_test/handler.clj
@@ -4,6 +4,16 @@
             [compojure.route :as route]
             [ring.middleware.json :as middleware]))
 
+(def animals (atom ()))
+
+(defn add-animal
+  [x]
+  (swap! animals conj x))
+
+(defn- get-animals [& _]
+  {:status 200
+   :body {:animals animals}})
+
 (defn- hello-world [& _]
   {:status 200
    :body {:hello "Hello World"}})
@@ -16,6 +26,8 @@
 (defroutes app-routes
   (GET "/sum/:x1/:x2" [x1 x2] (calculate-sum (read-string x1) (read-string x2)))
   (GET "/" [] hello-world)
+  (POST "/animal/:x1" [x1] (add-animal (read-string x1)))
+  (GET "/animals" [] get-animals)
   (route/resources "/")
   (route/not-found "Not Found"))
 
